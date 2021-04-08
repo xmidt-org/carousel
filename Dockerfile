da998dd9532e591eb71c6da1ac6e790d490386b4
Jack Murdock <kcajmagic@gmail.com>
2021-04-08 12:59:31 -0700

    replace __PROJECT__ with carousel

--- Dockerfile
+++ Dockerfile
@@ -25,10 +25,10 @@ RUN make test release
 FROM alpine:3.12.1
 
 COPY --from=builder /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/
-COPY --from=builder /src/__PROJECT__ /src/__PROJECT__.yaml /src/deploy/packaging/entrypoint.sh /go/bin/spruce /src/Dockerfile /src/NOTICE /src/LICENSE /src/CHANGELOG.md /
-COPY --from=builder /src/deploy/packaging/__PROJECT__.yaml /tmp/__PROJECT__.yaml
+COPY --from=builder /src/carousel /src/carousel.yaml /src/deploy/packaging/entrypoint.sh /go/bin/spruce /src/Dockerfile /src/NOTICE /src/LICENSE /src/CHANGELOG.md /
+COPY --from=builder /src/deploy/packaging/carousel.yaml /tmp/carousel.yaml
 
-RUN mkdir /etc/__PROJECT__/ && touch /etc/__PROJECT__/__PROJECT__.yaml && chmod 666 /etc/__PROJECT__/__PROJECT__.yaml
+RUN mkdir /etc/carousel/ && touch /etc/carousel/carousel.yaml && chmod 666 /etc/carousel/carousel.yaml
 
 USER nobody
 
@@ -39,4 +39,4 @@ EXPOSE 6601
 EXPOSE 6602
 EXPOSE 6603
 
-CMD ["/__PROJECT__"]
+CMD ["/carousel"]
--- deploy/packaging/entrypoint.sh
+++ deploy/packaging/entrypoint.sh
@@ -11,7 +11,7 @@ _is_sourced() {
 		&& [ "${FUNCNAME[1]}" = 'source' ]
 }
 
-# check arguments for an option that would cause /__PROJECT__ to stop
+# check arguments for an option that would cause /carousel to stop
 # return true if there is one
 _want_help() {
 	local arg
@@ -26,17 +26,17 @@ _want_help() {
 }
 
 _main() {
-	# if command starts with an option, prepend __PROJECT__
+	# if command starts with an option, prepend carousel
 	if [ "${1:0:1}" = '-' ]; then
-		set -- /__PROJECT__ "$@"
+		set -- /carousel "$@"
 	fi
-		# skip setup if they aren't running /__PROJECT__ or want an option that stops /__PROJECT__
-	if [ "$1" = '/__PROJECT__' ] && ! _want_help "$@"; then
-		echo "Entrypoint script for __PROJECT__ Server ${VERSION} started."
+		# skip setup if they aren't running /carousel or want an option that stops /carousel
+	if [ "$1" = '/carousel' ] && ! _want_help "$@"; then
+		echo "Entrypoint script for carousel Server ${VERSION} started."
 
-		if [ ! -s /etc/__PROJECT__/__PROJECT__.yaml ]; then
+		if [ ! -s /etc/carousel/carousel.yaml ]; then
 		  echo "Building out template for file"
-		  /spruce merge /__PROJECT__.yaml /tmp/__PROJECT___spruce.yaml > /etc/__PROJECT__/__PROJECT__.yaml
+		  /spruce merge /carousel.yaml /tmp/carousel_spruce.yaml > /etc/carousel/carousel.yaml
 		fi
 	fi
 
